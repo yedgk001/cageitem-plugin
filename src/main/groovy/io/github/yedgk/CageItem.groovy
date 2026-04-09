2080739d6517b22ef614718061af3ab675161fe3
yedgk <yedgk@proton.me>
2026-04-09 19:21:26 +0200

    Fixes

--- src/main/groovy/io/github/yedgk/CageItem.groovy
+++ src/main/groovy/io/github/yedgk/CageItem.groovy
@@ -10,10 +10,10 @@ class CageItem {
     static final def cageItem = new ItemBuilder(Material.CHAIN)
             .displayName(Component.text("Buda", NamedTextColor.YELLOW).decorate(TextDecoration.BOLD))
             .lore(
-                    Component.text("Po kliknięciu PPM na przeciwnika", NamedTextColor.WHITE),
-                    Component.text("tworzy z nim klatkę 1vs1 na 10 sekund", NamedTextColor.WHITE),
+                    Component.text("Po kliknięciu PPM na przeciwnika", NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false),
+                    Component.text("tworzy z nim klatkę 1vs1 na 10 sekund", NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false),
                     Component.text(""),
-                    Component.text("Czas przeładowania to ", NamedTextColor.WHITE).append(Component.text("1 minuta 20 sekund", NamedTextColor.LIGHT_PURPLE))
+                    Component.text("Czas przeładowania to ", NamedTextColor.WHITE).append(Component.text("1 minuta 20 sekund", NamedTextColor.LIGHT_PURPLE).decoration(TextDecoration.ITALIC, false))
             )
             .customModelData(981)
             .build()
--- src/main/groovy/io/github/yedgk/CageItemListener.groovy
+++ src/main/groovy/io/github/yedgk/CageItemListener.groovy
@@ -2,6 +2,7 @@ package io.github.yedgk
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.title.Title
 import org.bukkit.Bukkit
 import org.bukkit.Material
@@ -47,7 +48,7 @@ class CageItemListener implements Listener {
         long now = System.currentTimeMillis()
         def lastUsed = player.persistentDataContainer.get(key, PersistentDataType.LONG)
         if (now - lastUsed <= 80000L) {
-            player.sendActionBar(Component.text("Przedmiot jest jeszcze niedostępny!", NamedTextColor.DARK_RED))
+            player.sendActionBar(Component.text("Przedmiot jest jeszcze niedostępny!", NamedTextColor.DARK_RED).decoration(TextDecoration.ITALIC, false))
             return
         }
         player.persistentDataContainer.set(key, PersistentDataType.LONG, now)
@@ -70,7 +71,7 @@ class CageItemListener implements Listener {
             worldborder.warningDistance = 0
             it.worldBorder = worldborder
         }
-        p1.showTitle(Title.title(Component.text("Uwięziłeś gracza", NamedTextColor.LIGHT_PURPLE), Component.text("$p2.name")))
+        p1.showTitle(Title.title(Component.text("Uwięziłeś gracza", NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false), Component.text("$p2.name", NamedTextColor.LIGHT_PURPLE).decoration(TextDecoration.ITALIC, false)))
         Bukkit.scheduler.runTaskLater(plugin, {
             activeCages.remove(arena)
             [p1, p2].each { it.worldBorder = it.world.worldBorder }
